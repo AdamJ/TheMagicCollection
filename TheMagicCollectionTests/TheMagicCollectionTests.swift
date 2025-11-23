638b76f8503c05da410290a5b2cd6748ab562161
Adam Jolicoeur <adam.jolicoeur@gmail.com>
2025-11-23 15:41:26 -0500

    Initial Commit

---
+++ TheMagicCollectionTests/TheMagicCollectionTests.swift
@@ -0,0 +1,16 @@
+//
+//  TheMagicCollectionTests.swift
+//  TheMagicCollectionTests
+//
+//  Created by Adam Jolicoeur on 11/23/25.
+//
+
+import Testing
+
+struct TheMagicCollectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
